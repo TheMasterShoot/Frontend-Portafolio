b18b5cf407b8404bcfa90e8cb05e05e54bfcc959
TheMasterShoot <bryaan01@gmail.com>
2022-03-13 23:54:20 -0400

    creacion de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
 
-const routes: Routes = [];
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
+
+const routes: Routes = [
+  {path: '', component: AboutComponent},
+  {path: 'sobre-mi', component: AboutComponent},
+  {path: 'proyectos', component: ProjectsComponent},
+  {path: 'Crear-proyecto', component: CreateComponent},
+  {path: 'contacto', component: ContactComponent},
+  {path: '**', component: ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
